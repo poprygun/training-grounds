a94e99606e7b22e699b462a8c53521fa2694d31e
Alex Shumilov <ashumilov@pivotal.io>
2016-06-27 14:46:17 -0400

    add discovery service

--- dojo/src/main/java/io/pivotal/tg/DojoApplication.java
+++ dojo/src/main/java/io/pivotal/tg/DojoApplication.java
@@ -1,12 +1,20 @@
 package io.pivotal.tg;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DojoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DojoApplication.class, args);
 	}
+
+	private Log log = LogFactory.getLog(getClass());
+
+
 }
